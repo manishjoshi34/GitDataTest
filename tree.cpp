40d43de2d1d1388dc9bfd1aebdfb4299643ccdb0
Manish <34joshi@gmail.com>
2018-11-24 21:51:06 +0530

    new cpp files

---
+++ tree.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+	int data;
+	Node* next;
+	Node(){
+	next = NULL;
+	data = 0;
+	}
+	Node* addNode(int d,Node* head);
+	void printList(Node* head);
+};
+Node*  Node::addNode(int d,Node* head)
+{
+	Node* temp = new Node();
+	temp->data = d;
+	temp->next = head;
+	head=temp;
+return head;
+}
+void Node::printList(Node* head){
+	if(head==NULL)return;
+Node* temp=head;
+	while(temp!=NULL){
+		cout<<temp->data;
+		temp=temp->next;
+	}
+}
+
+int main(){
+	Node* head=new Node();
+	head->data=0;
+	head=head->addNode(1,head);
+	head=head->addNode(2,head);
+head->printList(head);
+cout<<"testing vim"<<endl;
+return 0;
+}
